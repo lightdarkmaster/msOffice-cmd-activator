8ac80f6ff949d61ac6a80b397385452dcd7abb9f
Christian Barbosa <christian.barbosa05222001@gmail.com>
2023-08-29 20:35:49 +0800

    First Commit

---
+++ Untitled-1.bat
@@ -0,0 +1,24 @@
+cd /d %ProgramFiles(x86)%\Microsoft Office\Office16
+cd /d %ProgramFiles%\Microsoft Office\Office16
+
+
+for /f %x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x"
+
+
+cscript ospp.vbs /setprt:1688
+cscript ospp.vbs /unpkey:6F7TH >nul
+cscript ospp.vbs /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH
+cscript ospp.vbs /sethst:e8.us.to
+cscript ospp.vbs /act
+
+---------------------------------------------------------------------------------------------------------
+
+IF ERROR/NOT WORK PLEASE VISIT THE WEBSITE BELOW and READ
+BỊ LỖI THÌ MỞ WEB BÊN DƯỚI và ĐỌC KĨ
+
+
+##https://msguides.com/office-2021
+
+##https://msguides.com/office-2021
+
+##https://msguides.com/office-2021
